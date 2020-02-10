e52da0da7c29dbdc345950bfdd7c75eeb03910a6
aLi <aaljamid@emich.edu>
2020-02-10 09:16:56 -0500

    Done with the first part

---
+++ js/gladiator.js
@@ -0,0 +1,8 @@
+export default class Gladiator {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+}
+
+// export default const test = "live hw";
---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// import  Gladiator from "./gladiator.js";
+// import test from "./gladiator.js";
+
+import Gladiator from "./gladiator.js";
+
+const max = new Gladiator("Maximus", "Trident");
+console.log(max.name); // "Maximus"
+console.log(max.weapon); // "Trident"
