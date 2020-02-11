354cdb58021bca3ef63b974988f5c6bd85245aab
Rawan991 <rawan.alrajeh@gmail.com>
2020-02-11 17:32:02 +0300

    Update

--- js/arena.js
+++ js/arena.js
@@ -1,6 +1,6 @@
 export default class Arena {
     constructor(name){
-      this.name = name.toUpperCase() 
+      this.name = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
        this.gladiators = []
     }
   
@@ -16,26 +16,27 @@ export default class Arena {
     fight(){
       const [gladiator1, gladiator2] = this.gladiators
       const [weapon1, weapon2] = [gladiator1.weapon, gladiator2.weapon]
-
-        if (weapon1 === 'Trident' && weapon2 ==='spear'){
+      console.log(weapon1)
+      console.log(weapon2)
+        if (weapon1 === 'Trident' && weapon2 ==='Spear'){
         console.log('Trident beats spear')
         this.gladiators.splice(1,1)
-        this.gladiator.pop();
+        //  this.gladiators.pop();
         }
-        if (weapon1 === 'Trident' && weapon2 === 'club'){
+        if (weapon1 === 'Trident' && weapon2 === 'Club'){
             console.log('Trident beats club')
             this.gladiators.splice(0,1)
-            this.gladiator.pop();
+            // this.gladiator.pop();
         }
-        if (weapon1 === 'spear' && weapon2 === 'Trident'){
+        if (weapon1 === 'Spear' && weapon2 === 'Trident'){
             console.log('spear beats Trident')
             this.gladiators.splice(1,1)
-            this.gladiator.pop();
+            // this.gladiator.pop();
         }
         if (weapon1 === 'club' && weapon2 === 'spear'){
             console.log('club beats spear')
             this.gladiators.splice(0,1)
-            this.gladiator.pop();
+            // this.gladiator.pop();
         }
     }
     entertain(){
--- js/index.js
+++ js/index.js
@@ -3,54 +3,54 @@
 import Arena from './arena.js'
 import  Gladiator from './gladiator.js'
 
-const max = new Gladiator("Maximus", "Trident");
-console.log(max.name); // "Maximus"
-console.log(max.weapon); // "Trident"
+// const max = new Gladiator("Maximus", "Trident");
+// console.log(max.name); // "Maximus"
+// console.log(max.weapon); // "Trident"
 
-//An arena has a name:
+// //An arena has a name:
 let colosseum = new Arena("Colosseum")
 console.log(colosseum.name) // => Colosseum
 
-//The name should be capitalized:
+// The name should be capitalized:
 colosseum = new Arena("Megalopolis")
 console.log(colosseum.name); // => Megalopolis
 
-//An arena can have gladiators:
-colosseum = new Arena("Colosseum");
-console.log(colosseum.gladiators); // => []
+// //An arena can have gladiators:
+// colosseum = new Arena("Colosseum");
+// console.log(colosseum.gladiators); // => []
 
 
 //You can add a gladiator to the arena :
 
 //max = new Gladiator("Maximus","Trident");
-colosseum = new Arena("Colosseum");
-colosseum.addGladiator(max);
- console.log(colosseum.gladiators); // => [Gladiator]
+// colosseum = new Arena("Colosseum");
+// colosseum.addGladiator(max);
+//  console.log(colosseum.gladiators); // => [Gladiator]
 
 
-//The arena should never have more than 2 gladiators in it at a time
+// //The arena should never have more than 2 gladiators in it at a time
 
 
-//max = new Gladiator("Maximus","Trident");
-const titus = new Gladiator("Titus","Sword");
-const andronicus = new Gladiator("Andronicus","Sword");
-colosseum = new Arena("Colosseum");
+// //max = new Gladiator("Maximus","Trident");
+// const titus = new Gladiator("Titus","Sword");
+// const andronicus = new Gladiator("Andronicus","Sword");
+// colosseum = new Arena("Colosseum");
 
-colosseum.addGladiator(max);
-colosseum.addGladiator(titus);
-colosseum.addGladiator(andronicus);
+// colosseum.addGladiator(max);
+// colosseum.addGladiator(titus);
+// colosseum.addGladiator(andronicus);
 
-console.log(colosseum.gladiators.length); // => 2
+// console.log(colosseum.gladiators.length); // => 2
 
-//fight:
+// //fight:
 
 
-// const max = new Gladiator("Maximus","Trident");
-// const titus = new Gladiator("Titus","Spear");
+const max = new Gladiator("Maximus","Trident");
+const titus = new Gladiator("Titus","Spear");
 // const colosseum = new Arena("Colosseum");
 
 colosseum.addGladiator(max);
 colosseum.addGladiator(titus);
-console.log(colosseum.fight());
+colosseum.fight();
 
 console.log(colosseum.gladiators); // => [max]
